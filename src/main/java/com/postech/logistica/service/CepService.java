b5a0a11ba3da0ee12e8818b110c3fc9862f98d35
Maikon Germano <mk_germano@hotmail.com>
2025-02-20 21:27:06 -0300

    fix: integracao

--- src/main/java/com/postech/logistica/service/CepService.java
+++ src/main/java/com/postech/logistica/service/CepService.java
@@ -1,24 +1,37 @@
 package com.postech.logistica.service;
 
-import org.json.JSONObject;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+
 @Service
+@RequiredArgsConstructor
 public class CepService {
 
-    private static final String API_URL = "https://viacep.com.br/ws/%s/json/";
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Value("${spring.google.maps.api-key}")
+    private String apiKey;
+
+    private static final String GOOGLE_MAPS_API_URL = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s";
 
     public double[] buscarLatitudeLongitude(String cep) {
         try {
-            String url = String.format(API_URL, cep);
-            RestTemplate restTemplate = new RestTemplate();
-            String response = restTemplate.getForObject(url, String.class);
-            JSONObject json = new JSONObject(response);
-            
-            double latitude = json.optDouble("latitude", 0.0);
-            double longitude = json.optDouble("longitude", 0.0);
-            
+            URI uri = new URI(String.format(GOOGLE_MAPS_API_URL, cep, apiKey));
+            String response = restTemplate.getForObject(uri, String.class);
+
+            JsonNode root = objectMapper.readTree(response);
+            JsonNode locationNode = root.path("results").path(0).path("geometry").path("location");
+
+            double latitude = locationNode.path("lat").asDouble();
+            double longitude = locationNode.path("lng").asDouble();
+
             return new double[]{latitude, longitude};
         } catch (Exception e) {
             System.err.println("Erro ao buscar coordenadas para o CEP " + cep + ": " + e.getMessage());
